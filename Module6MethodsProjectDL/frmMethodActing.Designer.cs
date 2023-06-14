bb1c222a1197b34ae58b6f1f61317996405e1387
zach scheffler <zscheffler.jtac@gmail.com>
2023-06-13 20:27:01 -0500

    Add project files.

---
+++ Module6MethodsProjectDL/frmMethodActing.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Module6MethodsProjectDL
+{
+    partial class frmMethodActing
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbxRight = new System.Windows.Forms.TextBox();
+            this.txtbxLeft = new System.Windows.Forms.TextBox();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.lblRight = new System.Windows.Forms.Label();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnPercentage = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtbxRight
+            // 
+            this.txtbxRight.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxRight.Location = new System.Drawing.Point(157, 42);
+            this.txtbxRight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtbxRight.Name = "txtbxRight";
+            this.txtbxRight.Size = new System.Drawing.Size(122, 32);
+            this.txtbxRight.TabIndex = 2;
+            // 
+            // txtbxLeft
+            // 
+            this.txtbxLeft.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxLeft.Location = new System.Drawing.Point(157, 3);
+            this.txtbxLeft.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtbxLeft.Name = "txtbxLeft";
+            this.txtbxLeft.Size = new System.Drawing.Size(122, 32);
+            this.txtbxLeft.TabIndex = 1;
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLeft.Location = new System.Drawing.Point(25, 6);
+            this.lblLeft.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(128, 25);
+            this.lblLeft.TabIndex = 2;
+            this.lblLeft.Text = "Left Operand";
+            // 
+            // lblRight
+            // 
+            this.lblRight.AutoSize = true;
+            this.lblRight.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRight.Location = new System.Drawing.Point(14, 42);
+            this.lblRight.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRight.Name = "lblRight";
+            this.lblRight.Size = new System.Drawing.Size(139, 25);
+            this.lblRight.TabIndex = 3;
+            this.lblRight.Text = "Right Operand";
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(8, 86);
+            this.btnPlus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(32, 37);
+            this.btnPlus.TabIndex = 3;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(67, 86);
+            this.btnSubtract.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(32, 37);
+            this.btnSubtract.TabIndex = 4;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(127, 86);
+            this.btnMultiply.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(32, 37);
+            this.btnMultiply.TabIndex = 5;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(186, 86);
+            this.btnDivide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(32, 37);
+            this.btnDivide.TabIndex = 6;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnPercentage
+            // 
+            this.btnPercentage.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPercentage.Location = new System.Drawing.Point(245, 86);
+            this.btnPercentage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPercentage.Name = "btnPercentage";
+            this.btnPercentage.Size = new System.Drawing.Size(32, 37);
+            this.btnPercentage.TabIndex = 7;
+            this.btnPercentage.Text = "%";
+            this.btnPercentage.UseVisualStyleBackColor = true;
+            this.btnPercentage.Click += new System.EventHandler(this.btnPercentage_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblAnswer.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(8, 134);
+            this.lblAnswer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(269, 84);
+            this.lblAnswer.TabIndex = 9;
+            this.lblAnswer.Text = "Answer Shows Here";
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(8, 226);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(122, 68);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(157, 226);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(121, 68);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMethodActing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(286, 302);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnPercentage);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.lblRight);
+            this.Controls.Add(this.lblLeft);
+            this.Controls.Add(this.txtbxLeft);
+            this.Controls.Add(this.txtbxRight);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmMethodActing";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Scheffler_Methods";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbxRight;
+        private System.Windows.Forms.TextBox txtbxLeft;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label lblRight;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnPercentage;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Module6MethodsProjectDL/frmMethodActing.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module6MethodsProjectDL
+{
+    public partial class frmMethodActing : Form
+    {
+        // Public Contsants to use
+        const byte ADD = 0;
+        const byte SUBTRACT = 1;
+        const byte MULTIPLY = 2;
+        const byte DIVIDE = 3;
+        const byte MODULUS = 4;
+
+        public frmMethodActing()
+        {
+            InitializeComponent();
+        }
+
+        public int methodMathematics(decimal operand1, decimal operand2, byte operation)
+        {
+            int result = 0;
+
+            switch (operation)
+            {
+                case ADD:
+                    result = (int)(operand1 + operand2);
+                    break;
+                    case SUBTRACT:
+                    result = (int)(operand1 - operand2);
+                    break;
+                    case MULTIPLY:
+                    result = (int)(operand1 * operand2);
+                    break;
+                    case DIVIDE:
+                    result = (int)(operand1 / operand2);
+                    break;
+                    case MODULUS:
+                    result = (int)(operand1 % operand2);
+                    break;
+            }
+
+            return result;
+        }
+
+        private void btnPercentage_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = decimal.Parse(txtbxLeft.Text);
+            decimal operand2 = decimal.Parse(txtbxRight.Text);
+            byte operation = MODULUS;
+
+            int result = methodMathematics(operand1, operand2, operation);
+
+            lblAnswer.Text = $"{operand1} % {operand2} = {result}";
+
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = decimal.Parse(txtbxLeft.Text);
+            decimal operand2 = decimal.Parse(txtbxRight.Text);
+            byte operation = DIVIDE;
+
+            int result = methodMathematics(operand1, operand2, operation);
+
+            lblAnswer.Text = $"{operand1} / {operand2} = {result}";
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = decimal.Parse(txtbxLeft.Text);
+            decimal operand2 = decimal.Parse(txtbxRight.Text);
+            byte operation = MULTIPLY;
+
+            int result = methodMathematics(operand1, operand2, operation);
+
+            lblAnswer.Text = $"{operand1} * {operand2} = {result}";
+
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = decimal.Parse(txtbxLeft.Text);
+            decimal operand2 = decimal.Parse(txtbxRight.Text);
+            byte operation = SUBTRACT;
+
+            int result = methodMathematics(operand1, operand2, operation);
+
+            lblAnswer.Text = $"{operand1} - {operand2} = {result}";
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            decimal operand1 = decimal.Parse(txtbxLeft.Text);
+            decimal operand2 = decimal.Parse(txtbxRight.Text);
+            byte operation = ADD;
+
+            int result = methodMathematics(operand1, operand2, operation);
+
+            lblAnswer.Text = $"{operand1} + {operand2} = {result}";
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtbxLeft.Text = "";
+            txtbxRight.Text = "";
+            lblAnswer.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
